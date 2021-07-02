343626dee64da3b1508313750f61510042cd2834
Durgaprasath01415 <83449179+Durgaprasath01415@users.noreply.github.com>
2021-07-02 09:54:34 +0530

    ADDED : Dailywage and totalwage calculation and store it

--- JavaScript/employeewage.js
+++ JavaScript/employeewage.js
@@ -4,7 +4,7 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20; 
 const NUM_OF_WORKING_DAYS = 20;
-const MAX_HRS_IN_MONTH = 100;
+const MAX_HRS_IN_MONTH = 160;
 
 function getWorkingHours(empCheck) {
     switch(empCheck){
@@ -17,16 +17,22 @@ function getWorkingHours(empCheck) {
     }
 }
 
+function calcWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
+
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+let empWageArray = new Array();
 
 while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+    let empCheck = Math.floor((Math.random() * 10) % 3);
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empWageArray.push(calcWage(empHrs)); 
 }
 
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
+let empWage = calcWage(totalEmpHrs);
 
-console.log("Total working days: " + totalWorkingDays);
-console.log("Total employee hours = " + totalEmpHrs+" Total employee wage = " + empWage);
+console.log("Total Employee Working Hours : " + totalEmpHrs + "\nTotal Employee Working Days :  " + totalWorkingDays + "\nTotal Employee Wage : " + empWage);
